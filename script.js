76eecd943a0b99cc8fbd5de402a000b189e52a22
Abhishek <abhishekbhikule76@gmail.com>
2023-01-10 23:27:56 +0530

    added the array of info

--- script.js
+++ script.js
@@ -44,6 +44,44 @@ class UI {
   }
 }
 
+//Address Class
+class Addresses {
+  constructor(
+    id,
+    firstName,
+    lastName,
+    email,
+    phone,
+    streetAddr,
+    postCode,
+    city,
+    country,
+    labels
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.streetAddr = streetAddr;
+    this.postCode = postCode;
+    this.city = city;
+    this.country = country;
+    this.labels = labels;
+  }
+
+  static getAddresses() {
+    // from locslStaorage
+    let addresses;
+    if (localStorage.getItem("addresses") == null) {
+      addresses = [];
+    } else {
+      addresses = JSON.parse(localStorage.getItem("addresses"));
+    }
+    return addresses;
+  }
+}
+
 // ------------------------------
 window.addEventListener("DOMContentLoaded", () => {
   loadJson(); //loading the countirs json
@@ -85,7 +123,125 @@ function eventListener() {
   modalBtns.addEventListener("click", (e) => {
     e.preventDefault();
     if (e.target.id == "save-btn") {
-      let isFormValid = getFprmData();
+      let isFormValid = getFormData();
+      if (!isFormValid) {
+        form.querySelectorAll("input").forEach((input) => {
+          setTimeout(() => {
+            input.classList.remove("errorMsg");
+          }, 1500);
+        });
+      } else {
+        let allItem = Addresses.getAddresses(); //this will create an empty array
+        console.log(allItem);
+      }
     }
   });
+}
+
+// Get form data
+function getFormData() {
+  let inputValidStatus = [];
+  // console.log(
+  //   form.addr_ing_name.value,
+  //   form.first_name.value,
+  //   form.last_name.value,
+  //   form.email.value,
+  //   form.phone.value,
+  //   form.street_addr.value,
+  //   form.postal_code.value,
+  //   form.city.value,
+  //   form.country.value
+  // );
+  if (
+    !strRegex.test(form.addr_ing_name.value) ||
+    form.addr_ing_name.value.trim().length == 0
+  ) {
+    errMsg(form.addr_ing_name);
+    inputValidStatus[0] = false;
+  } else {
+    addrName = form.addr_ing_name.value;
+    inputValidStatus[0] = true;
+  }
+
+  if (
+    !strRegex.test(form.first_name.value) ||
+    form.first_name.value.trim().length == 0
+  ) {
+    errMsg(form.first_name);
+    inputValidStatus[1] = false;
+  } else {
+    firstName = form.first_name.value;
+    inputValidStatus[1] = true;
+  }
+
+  if (
+    !strRegex.test(form.last_name.value) ||
+    form.last_name.value.trim().length == 0
+  ) {
+    errMsg(form.last_name);
+    inputValidStatus[2] = false;
+  } else {
+    lastName = form.last_name.value;
+    inputValidStatus[2] = true;
+  }
+
+  if (!emailRegex.test(form.email.value)) {
+    errMsg(form.email);
+    inputValidStatus[3] = false;
+  } else {
+    email = form.email.value;
+    inputValidStatus[3] = true;
+  }
+
+  if (!phoneRegex.test(form.phone.value)) {
+    errMsg(form.phone);
+    inputValidStatus[4] = false;
+  } else {
+    phone = form.phone.value;
+    inputValidStatus[4] = true;
+  }
+
+  if (!(form.street_addr.value.trim().length > 0)) {
+    errMsg(form.street_addr);
+    inputValidStatus[5] = false;
+  } else {
+    streetAddr = form.street_addr.value;
+    inputValidStatus[5] = true;
+  }
+
+  if (!digitRegex.test(form.postal_code.value)) {
+    errMsg(form.postal_code);
+    inputValidStatus[6] = false;
+  } else {
+    postCode = form.postal_code.value;
+    inputValidStatus[6] = true;
+  }
+
+  if (!strRegex.test(form.city.value) || form.city.value.trim().length == 0) {
+    errMsg(form.city);
+    inputValidStatus[7] = false;
+  } else {
+    city = form.city.value;
+    inputValidStatus[7] = true;
+  }
+
+  // console.table(
+  //   addrName,
+  //   firstName,
+  //   lastName,
+  //   email,
+  //   phone,
+  //   streetAddr,
+  //   postCode,
+  //   city,
+  //   country,
+  //   labels
+  // );
+  country = form.country.value;
+  labels = form.labels.value;
+  return inputValidStatus.includes(false) ? false : true;
+}
+
+function errMsg(inputBox) {
+  inputBox.classList.add("errorMsg");
 }
